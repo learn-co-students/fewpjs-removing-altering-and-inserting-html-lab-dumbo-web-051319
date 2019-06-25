f5e8354d4d0ebe52d768e112330aed86a511661b
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-25 00:05:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.querySelector("#main")
+let  newHeader = document.createElement("h1")
+
+main.remove()
+newHeader.id= 'victory'
+newHeader.innerHTML = "Momo is the champion"
