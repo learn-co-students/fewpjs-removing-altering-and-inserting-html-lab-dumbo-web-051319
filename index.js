9ed6cf6c6712bf448aab75f455e82183994ccf1e
L.R.L <DaleLoreNY@gmail.com>
2019-06-25 09:25:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+// document.getElementById('main').remove()
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "DaleLore is the champion"
